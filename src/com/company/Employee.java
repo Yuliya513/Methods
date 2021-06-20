29c957572c95987c39347db3f797891bc761c15f
Yuliya513 <83278354+Yuliya513@users.noreply.github.com>
2021-06-20 11:23:50 +0300

    online project with some methods

---
+++ src/com/company/Employee.java
@@ -0,0 +1,58 @@
+package com.company;
+
+public class Employee {
+    /*
+    String name;
+    int rate;
+    int salary;
+    int bonuses;
+
+    public static void main(String[] args) {
+	Account c=new Account ();
+	c.SetName ("Ivan");
+	c.Setsalary (500);
+	c. Setrate (10);
+	c.Setbonuses (2);
+	System.out.println (c.getName ());
+	System.out.println (c.getsalary ());
+	System.out.println (c.getrate ());
+	System.out.println (c.getbonuses ());
+
+    }
+}
+public void setName() {
+    setName("Ivan");
+}
+    
+
+    public void setName (String Name)
+{
+    this. name=name;
+}
+    public void setSalary (int salary)
+    {
+        this.salary=salary;
+    }
+    public void setrate (int rate)
+    {
+        this.rate=rate;
+    }
+    public void setbonuses (int bonuses)
+    {
+        this. bonuses=bonuses;
+    }
+    public String getName (){
+    return this.name;
+    }
+    public Integer getSalary (){
+        return this.salary;
+    }
+    public Integer getrate (){
+        return this.rate;
+    }
+    public Integer getbonuses (){
+        return this.bonuses;
+    }
+
+     */
+}
